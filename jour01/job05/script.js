7c55e29fff49f03478a3abde46e8346453087882
Hajar-aslaan <hajar.elmoudden.88@gmail.com>
2023-03-01 15:48:11 +0100

    ajoute un job

---
+++ jour01/job05/script.js
@@ -0,0 +1,8 @@
+const jourssemaines = ["Lundi", "Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+function afficherjourssemaines() {
+  joursemaines.forEach(function (jour) {
+    console.log(jour)
+  })
+}
+
+afficherjourssemaines();
